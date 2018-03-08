56fdf9e41c4462b9e78b821bcace1e458938bab6
Kazuki Hanai <cs16075@s.inf.shizuoka.ac.jp>
2018-03-08 17:25:29 +0900

    add get/pic

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from bottle import route, run, view, static_file, url
-from bottle import get, request
+from bottle import get, request, response
 import os
 #import picamera
 import pygame.mixer
@@ -12,6 +12,7 @@ sound1 = pygame.mixer.Sound("./take1.wav")
 #camera = picamera.PiCamera()
 
 id = 0
+save_path = "./img/"
 
 @route('/start')
 def main():
@@ -22,7 +23,7 @@ def main():
 @route('/status/<id>')
 def status(id):
     #take picture
-    save_path = "./img/"
+    global save_path
     pic_name  =  id + ".jpg"
     pic_loc   = save_path + pic_name
     #camera.resolution = (1024,768) 
@@ -35,16 +36,24 @@ def status(id):
 
     #sound2.play()	
     #common.pic_print(pic_loc)
-    
+
     return pic_loc
 
 @route('/get_pic/<id>')
 def get_pic(id):
+    global save_path
     pic_name = id + ".jpg"
-    if os.path.isfile(pic_name):
-        return "file exists"
-    return "file do not exists"
+    pic_loc = save_path + pic_name
 
+    if not os.path.isfile(pic_loc):
+        return "file do not exists"
+
+    response.content_type = 'image/jpg'
+    with open(pic_loc, 'rb') as fh:
+        content = fh.read()
+        response.set_header('Content-Length', str(len(content)))
+        return content
+    #return "faile do not exists"
 
 #TODO:localhost setting
 run(host='localhost', port=8080, debug=True)
